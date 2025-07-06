a03bf7582f7723cec350147000ae5a9f560eeb15
V3NO <jalilchebli9@gmail.com>
2025-07-06 16:50:09 +0100

    new update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://caarashpaxencctvhquf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhYXJhc2hwYXhlbmNjdHZocXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMjIxNDgsImV4cCI6MjA2NDc5ODE0OH0._1O99lF5CIuYAefN147R8vyFYC4FI_JnjwMYXsLX1BQ";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
